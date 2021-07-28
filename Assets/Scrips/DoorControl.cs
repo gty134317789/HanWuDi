a38f9b01f04f389f9b4abf7c6615b3af0ecbb399
PK-wwj <2795024144@qq.com>
2021-07-28 18:19:23 +0800

    开门关门动画以及触发
    将一个门的预制体变为普通物体，删除门板并重新制作无材质的门板，创建动画文件夹animation，存入动画OpenDoor以及CloseDoor。创建文件夹scripts，其中创建脚本DoorControl

---
+++ Assets/Scrips/DoorControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl: MonoBehaviour
+{
+
+    private Animation ani;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //得到第一个子物体的动画控件
+        ani = transform.GetChild(0).GetComponent<Animation>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ani.Play("OpenDoor");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ani.Play("CloseDoor");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
