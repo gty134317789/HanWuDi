f9e5d08b64d86e54f7554bcca6254891533c6302
PK_wwj <2795024144@qq.com>
2021-08-13 17:26:59 +0800

    增加了转场动画
    增加了预制体Plane（8）内含Crossfade（原名canvas），与gameobject（脚本在这个上面）。
    新增脚本Switch view，参数Transition选择crossfade。
    脚本Splash_Scr***为携程参考脚本，没啥作用，用于参考所以没删。
    新增两个动画分别为crossfade_start与crossfade_end,都在文件夹animation下。
    panel（8）的显示的图片是新创建的一个纹理，此纹理使得panel（8）显示的图片为新建摄像机new所观测的图像，可更改

---
+++ Assets/Scrips/SwitchView.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class SwitchView : MonoBehaviour
+{
+
+    public Animator transition;
+    public float transitionTime=2f;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hello");
+        StartCoroutine(LoadLevel(1));//参数只能为int
+
+    }
+    
+    //创建携程播放动画
+    IEnumerator LoadLevel(int levelIndex)
+    {
+
+        //播放相应动画
+        transition.SetTrigger("Start");
+
+
+        //等待
+        yield return new WaitForSeconds(transitionTime);//停止这个携程等待一段时间后继续
+        //加载新场景
+
+        SceneManager.LoadScene(levelIndex);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+       
+
+    }
+
+    //控制两次按E的时间间隔
+    //
+    //
+   
+
+    // Update is called once per frame
+   
+}
+
