67fbeaa958212024493ce8c90fa002446b3697e0
PK_wwj <2795024144@qq.com>
2021-10-01 17:07:02 +0800

    修正了焊板，增加了清点物品提示
    提示脚本添加在checkequipment脚本中，给焊板新增父物体help，焊板相对父物体的坐标须保持不变，修改焊板位置需要通过移动父物体help来达成

--- Assets/Scrips/CheckEquipment.cs
+++ Assets/Scrips/CheckEquipment.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CheckEquipment : MonoBehaviour
 {
+    private Text m_MyText;           //字体组件
     public Transform tr;
     public int ScoreOfCheck; //存储清点设备阶段分数
     public Collider ElectricBox;  //电箱
@@ -27,6 +29,8 @@ public class CheckEquipment : MonoBehaviour
     int NumOfSteelPlate;
     int NumOfSandpaper;
 
+    public string num;
+
     GameObject Button_StartWeld;//开始焊接按钮
 
     //焊接前挑选的物品
@@ -36,15 +40,21 @@ public class CheckEquipment : MonoBehaviour
     public GameObject ToSandpaper_1;   //待用砂纸1
     public GameObject ToSandpaper_2;   //待用砂纸2
 
+    //private Text m_MyText;           //字体组件
+
 
     void Start()
     {
-
+        GameObject root = GameObject.Find("Canvas");
+        m_MyText=root.transform.Find("Image/Text").GetComponent<Text>();
+        m_MyText.text = "点击物品清点15个设备\n点击按钮即可开始焊接";
+        string num = ScoreOfCheck.ToString();
     }
 
 
     void Update()
     {
+        
         //点击鼠标左键之后
         if (Input.GetMouseButtonDown(0))
         {
@@ -64,133 +74,164 @@ public class CheckEquipment : MonoBehaviour
                 if (hit.collider.gameObject.name == "电箱")
                 {
                     ScoreOfCheck++;  //加1分
+                    num = ScoreOfCheck.ToString();
                     ElectricBox = GameObject.Find("电箱").GetComponent<Collider>();
                     ElectricBox.enabled = false;
                     Debug.Log(ScoreOfCheck);
+                    m_MyText.text = "电箱已清点\n"+ num+"-15";
+
                 }
 
                 if (hit.collider.gameObject.name == "焊枪")
                 {
                     ScoreOfCheck++;  //加1分
+                    num = ScoreOfCheck.ToString();
                     WeldingTorch = GameObject.Find("焊枪").GetComponent<Collider>();
                     WeldingTorch.enabled = false;
                     Debug.Log(ScoreOfCheck);
+                    m_MyText.text = "焊枪已清点\n" + num + "-15";
                 }
 
                 if (hit.collider.gameObject.name == "夹子")
                 {
                     ScoreOfCheck++;  //加1分
+                    num = ScoreOfCheck.ToString();
                     WeldingClamp = GameObject.Find("夹子").GetComponent<Collider>();
                     WeldingClamp.enabled = false;
                     Debug.Log(ScoreOfCheck);
+                    m_MyText.text = "夹子已清点\n" + num + "-15";
                 }
 
                 if (hit.collider.gameObject.name == "电焊机")
                 {
                     ScoreOfCheck++;  //加1分
+                    num = ScoreOfCheck.ToString();
                     WeldingMachine = GameObject.Find("电焊机").GetComponent<Collider>();
                     WeldingMachine.enabled = false;
                     Debug.Log(ScoreOfCheck);
+                    m_MyText.text = "电焊机已清点\n" + num + "-15";
                 }
 
                 if (hit.collider.gameObject.name == "仪表盘1")
                 {
                     ScoreOfCheck++;  //加1分
+                    num = ScoreOfCheck.ToString();
                     Dashboard_1 = GameObject.Find("仪表盘1").GetComponent<Collider>();
                     Dashboard_1.enabled = false;
                     Debug.Log(ScoreOfCheck);
+                    m_MyText.text = "仪表盘1已清点\n" + num + "-15";
                 }
 
                 if (hit.collider.gameObject.name == "仪表盘2")
                 {
                     ScoreOfCheck++;  //加1分
+                    num = ScoreOfCheck.ToString();
                     Dashboard_2 = GameObject.Find("仪表盘2").GetComponent<Collider>();
                     Dashboard_2.enabled = false;
                     Debug.Log(ScoreOfCheck);
+                    m_MyText.text = "仪表盘2已清点\n" + num + "-15";
                 }
 
                 if (hit.collider.gameObject.name == "电线")
                 {
                     ScoreOfCheck++;  //加1分
+                    num = ScoreOfCheck.ToString();
                     Wire = GameObject.Find("电线").GetComponent<Collider>();
                     Wire.enabled = false;
                     Debug.Log(ScoreOfCheck);
+                    m_MyText.text = "电线已清点\n" + num + "-15";
                 }
 
                 if (hit.collider.gameObject.name == "焊条1")
                 {
                     ScoreOfCheck++;  //加1分
+                    num = ScoreOfCheck.ToString();
                     Weldingrod_1 = GameObject.Find("焊条1");
                     Weldingrod_1.SetActive(false);
                     ToWeldingrod_1.SetActive(true);
                     NumOfWeldinggrod++;
                     Debug.Log(ScoreOfCheck);
+                    m_MyText.text = "焊条1已清点\n" + num + "-15";
                 }
 
                 if (hit.collider.gameObject.name == "焊条2")
                 {
                     ScoreOfCheck++;  //加1分
+                    num = ScoreOfCheck.ToString();
                     Weldingrod_2 = GameObject.Find("焊条2");
                     Weldingrod_2.SetActive(false);
                     ToWeldingrod_2.SetActive(true);
                     NumOfWeldinggrod++;
                     Debug.Log(ScoreOfCheck);
+                    m_MyText.text = "焊条2已清点\n" + num + "-15";
                 }
 
                 if (hit.collider.gameObject.name == "焊条3")
                 {
                     ScoreOfCheck++;  //加1分
+                    num = ScoreOfCheck.ToString();
                     Weldingrod_3 = GameObject.Find("焊条3");
                     Weldingrod_3.SetActive(false);
                     ToWeldingrod_3.SetActive(true);
                     NumOfWeldinggrod++;
                     Debug.Log(ScoreOfCheck);
+                    m_MyText.text = "焊条3已清点\n" + num + "-15";
                 }
 
                 if (hit.collider.gameObject.name == "焊板1")
                 {
                     ScoreOfCheck++;  //加1分
+                    num = ScoreOfCheck.ToString();
                     SteelPlate_1 = GameObject.Find("焊板1");
                     SteelPlate_1.SetActive(false);
                     NumOfSteelPlate++;
                     Debug.Log(ScoreOfCheck);
+                    m_MyText.text = "焊板1已清点\n" + num + "-15";
                 }
 
                 if (hit.collider.gameObject.name == "焊板2")
                 {
                     ScoreOfCheck++;  //加1分
+                    num = ScoreOfCheck.ToString();
                     SteelPlate_2 = GameObject.Find("焊板2");
                     SteelPlate_2.SetActive(false);
                     NumOfSteelPlate++;
                     Debug.Log(ScoreOfCheck);
+                    m_MyText.text = "焊板2已清点\n" + num + "-15";
                 }
 
                 if (hit.collider.gameObject.name == "砂纸1")
                 {
                     ScoreOfCheck++;  //加1分
+                    num = ScoreOfCheck.ToString();
                     Sandpaper_1 = GameObject.Find("砂纸1");
                     Sandpaper_1.SetActive(false);
                     ToSandpaper_1.SetActive(true);
                     NumOfSandpaper++;
                     Debug.Log(ScoreOfCheck);
+                    m_MyText.text = "砂纸1已清点\n" + num + "-15";
                 }
 
                 if (hit.collider.gameObject.name == "砂纸2")
                 {
                     ScoreOfCheck++;  //加1分
+                    num = ScoreOfCheck.ToString();
                     Sandpaper_2 = GameObject.Find("砂纸2");
                     Sandpaper_2.SetActive(false);
                     ToSandpaper_2.SetActive(true);
                     NumOfSandpaper++;
                     Debug.Log(ScoreOfCheck.ToString());
+                    m_MyText.text = "砂纸2已清点\n" + num + "-15";
                 }
 
                 if (hit.collider.gameObject.name == "台灯")
                 {
                     ScoreOfCheck++;  //加1分
+                    num = ScoreOfCheck.ToString();
                     Light = GameObject.Find("台灯").GetComponent<Collider>();
                     Light.enabled = false;
                     Debug.Log(ScoreOfCheck.ToString() + NumOfSandpaper.ToString() + NumOfSteelPlate.ToString() + NumOfWeldinggrod.ToString());
+                    m_MyText.text = "台灯已清点\n" + num + "-15";
                 }
                 if (hit.collider.gameObject.name == "开始焊接按钮")
                 {
